703e4c4dbeece5208877e35b2a45547ef6d29b0c
AlgohashEric <Eric@algohash.org>
2025-03-21 18:20:33 +0800

    add

---
+++ progressier.js
@@ -0,0 +1,4 @@
+// This file exists to redirect to the proper Progressier script
+// Include this in your HTML instead: <script defer src="https://progressier.app/UZGuNyQKREIGp7udYESO/script.js"></script>
+
+window.location.href = "https://progressier.app/UZGuNyQKREIGp7udYESO/script.js"; 
